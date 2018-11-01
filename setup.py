8e801cca7e5cc2f39f38db65c753d4391f37bb27
Damir <sircco@gmail.com>
2018-11-01 19:34:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests==2.11.1', 'future', 'mock'],
+    install_requires=['requests==2.20.0', 'future', 'mock'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
